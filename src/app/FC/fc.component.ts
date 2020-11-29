2789504d67215032dfd3b1b33991f0b697ec22e3
urmil <ucodes28@gmail.com>
2020-11-29 10:41:29 -0600

    created first component manually inside the root component and registered the component in the declarations array inside the app.module.ts file

---
+++ src/app/FC/fc.component.ts
@@ -0,0 +1,13 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-fc',
+  templateUrl: './fc.component.html'
+})
+
+
+
+export class FcComponent {
+  footballclubs = ['Feynoord', 'PSV', 'Ajax'];
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FcComponent} from './FC/fc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FcComponent
   ],
   imports: [
     BrowserModule,
