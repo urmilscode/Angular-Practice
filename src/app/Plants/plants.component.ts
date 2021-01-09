30c43adfa607c7ec459eb55bc61baabe3c8d1f1e
urmil <ucodes28@gmail.com>
2021-01-09 12:32:42 -0600

    implemented all four forms of databinding, noticed difference between event binding and ngModel in plant component

--- src/app/Plants/plants.component.ts
+++ src/app/Plants/plants.component.ts
@@ -5,8 +5,19 @@ import {Component} from '@angular/core';
   templateUrl: './plants.component.html'
 })
 export class PlantComponent {
+  detailData = '';
+  plantDetails = '';
+  details = 'Yup plants are the best';
   plantList = ['Christmas', 'Avacado', 'Bamboo'];
   getPlantData(event: Event) {
     console.log(event);
+    this.plantDetails = (<HTMLInputElement> event.target).value;
+  }
+  getDetails() {
+    this.detailData = 'Plants provide more oxygen';
+  }
+  getDetailData(event: Event) {
+    console.log(event);
+    this.detailData = (<HTMLInputElement> event.target).value;
   }
 }
